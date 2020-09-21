8b356faa40929152fc6012fe4c7171548e79aa73
futirstanjo <junior_santos2100@outlook.com>
2020-09-20 21:43:56 -0300

    Criada configuracao inicial para connectar com o mongo.

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,6 @@
+import * as mongoose from 'mongoose'
 import * as restify from 'restify'
+
 import { environment } from '../common/environment'
 import { Router } from '../common/router'
 
@@ -6,6 +8,13 @@ import { Router } from '../common/router'
 export class Server {
 
   application: restify.Server
+
+  initializeDb() {
+    return mongoose.connect(environment.db.url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  }
 
   initRoutes(routers: Router[]): Promise<any> {
     return new Promise((resolve, reject) => {
@@ -30,7 +39,9 @@ export class Server {
   }
 
   bootstrap(routers: Router[] = []): Promise<Server> {
-    return this.initRoutes(routers)
-      .then(() => this)
+    return this.initializeDb().then(() => (
+      this.initRoutes(routers)
+        .then(() => this)
+    ))
   }
 }
