624b06945761642f0ad23897116a183b6d002894
futirstanjo <junior_santos2100@outlook.com>
2020-09-23 21:50:37 -0300

    Commit inicial

--- common/environment.ts
+++ common/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   server: { port: process.env.SERVER_PORT || 3000 },
-  db: { url: process.env.DB_URL || "mongodb+srv://futirstanjo:<COITADO>@cluster0.lajjj.gcp.mongodb.net/meat-api" }
+  db: { url: process.env.DB_URL || "mongodb+srv://futirstanjo:<teste>@cluster0.lajjj.gcp.mongodb.net/meat-api" }
 }
